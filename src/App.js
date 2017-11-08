ddeac0f7082bb3beb08c751c4e4fc50420cc19bd
Davi Augusto <Davi Augusto>
2017-11-08 16:07:25 -0200

    Criação dos componentes da home e importação de assets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import HeaderHome from './HeaderHome';
+import AnunciosHome from './AnunciosHome';
+import FooterHome from './FooterHome';
+import LinkCategoria from './LinkCategoria';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <HeaderHome />
+        <div className='container'>
+          <h3>Últimos anúncios</h3>
+          <div className='row'>
+            <AnunciosHome />
+            <AnunciosHome />
+            <AnunciosHome />
+            <AnunciosHome />
+          </div>
+          <h3>Categorias</h3>
+          <div className='row'>
+            <LinkCategoria />
+            <LinkCategoria />
+            <LinkCategoria />
+            <LinkCategoria />
+          </div>
+        </div>
+        <FooterHome />
       </div>
     );
   }
---
+++ src/FooterHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterHome = props => (
+    <footer className="py-5 bg-dark mt-4">
+        <div className="container">
+            <p className="m-0 text-center text-white">2017</p>
+        </div>
+    </footer>
+);
+
+export default FooterHome;
