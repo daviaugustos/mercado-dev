f99520fa9a750be0b72da9decbe494ee7e344fc3
Davi Augusto <Davi Augusto>
2017-11-09 13:24:41 -0200

    Correção do caminho do arquivo no gitignore

--- src/database/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCNZTWNX8cAC7LyCe-SQ7zZIpaj_HOOYjc",
-    authDomain: "mercadodev-2da58.firebaseapp.com",
-    databaseURL: "https://mercadodev-2da58.firebaseio.com",
-    projectId: "mercadodev-2da58",
-    storageBucket: "mercadodev-2da58.appspot.com",
-    messagingSenderId: "927973691484"
-}
